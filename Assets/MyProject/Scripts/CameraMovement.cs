42e54951adb5c510286e1fe4540af93b88850767
Kauan Paixão <kauan126paixao@gmail.com>
2024-11-13 22:28:03 -0300

    Add movimentação player e câmera
    *correção na rotação do player via cursor

---
+++ Assets/MyProject/Scripts/CameraMovement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    GameObject player;
+    bool followPlayer = true;
+
+    void Start() 
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update() 
+    {
+        if (followPlayer == true)
+        {
+            canFollowPlayer();
+        }
+    }
+
+    public void setFollowPlayer(bool val) 
+    {
+        followPlayer = val;
+    }
+
+    void canFollowPlayer() 
+    {
+        Vector3 newPosition = new Vector3(player.transform.position.x, player.transform.position.y, this.transform.position.z);
+        this.transform.position = newPosition;
+    }
+}
--- Assets/MyProject/Scripts/PlayerController.cs
+++ Assets/MyProject/Scripts/PlayerController.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    void Update()
+    public bool Moving = false;
+    float Speed = 5.0f;
+
+    Vector3 mousePos;
+    Camera cam;
+    Rigidbody2D rb;
+
+    private void Start()
     {
-        // Obtém a posição do mouse em relação ao mundo
-        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        rb = GetComponent<Rigidbody2D>();
+        cam = Camera.main;
+        
+    }
 
-        // Calcula o ângulo entre o objeto e o mouse
-        float angle = Mathf.Atan2(mousePosition.y - transform.position.y, mousePosition.x - transform.position.x) * Mathf.Rad2Deg;
+    private void Update()
+    {
+        rotateToCamera();
+        Movement();
+    }
 
-        // Define a rotação do objeto
-        transform.rotation = Quaternion.Euler(0, 0, angle);
+    void rotateToCamera() 
+    {
+        mousePos = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z - cam.transform.position.z));
+        rb.transform.eulerAngles = new Vector3(0, 0, Mathf.Atan2((mousePos.y - transform.position.y), (mousePos.x - transform.position.x)) * Mathf.Rad2Deg);
+    }
+
+    void Movement() 
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.up * Speed * Time.deltaTime, Space.World);
+            Moving = true;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * Speed * Time.deltaTime, Space.World);
+            Moving = true;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.down * Speed * Time.deltaTime, Space.World);
+            Moving = true;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * Speed * Time.deltaTime, Space.World);
+            Moving = true;
+        }
+        if (Input.GetKey(KeyCode.D) != true && Input.GetKey(KeyCode.A) != true && Input.GetKey(KeyCode.S) != true && Input.GetKey(KeyCode.W) != true) 
+        {
+            Moving = false;
+        }
     }
 }
