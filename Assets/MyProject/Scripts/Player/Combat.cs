ac161dbb41fbcbc9cab8b4120f2192d1a27e3a7c
Kauan Paixão <kauan126paixao@gmail.com>
2025-02-10 20:46:19 -0300

    Teste1
    Exemplo para criação de script e teste

---
+++ Assets/MyProject/Scripts/Player/Combat.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Combat : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Exemplo de criação de script na unity
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
