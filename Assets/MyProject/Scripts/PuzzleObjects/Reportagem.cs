aecc08969a4732648ff4a7f3fdcf3f6877dbb6e3
Kauan Paixão <kauan126paixao@gmail.com>
2025-03-21 20:09:35 -0300

    Puzzle da Reportagem
    Implementando o puzzle da reportagem.

--- Assets/MyProject/Scripts/PuzzleObjects/Reportagem.cs
+++ Assets/MyProject/Scripts/PuzzleObjects/Reportagem.cs
@@ -3,44 +3,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Armarinho : NPCs
+public class Reportagem : NPCs
 {
     [SerializeField] private PopUpObject popUpObject;
     [SerializeField] private InventorySO playerInventory;
-    [SerializeField] private ItemSO chaveArmarinho;
-    [SerializeField] private ItemSO recompensa;
-    [SerializeField] private int quantidadeRecompensa = 1;
+    [SerializeField] private ItemSO pilha;
     [SerializeField] private DialogueDataSO posRecompensa;
     [SerializeField] private DialogueDataSO interacaoPosRecompensa;
     private bool popUpAtivado = false;
-    private bool armarinhoAberto = false; // Impede que o pop-up apareça após abrir o armarinho
+    private bool tvLigada = false; // Impede que o pop-up apareça após ligar a Tv
 
     protected override void DialogueEnded()
     {
         base.DialogueEnded();
-        if (armarinhoAberto) return;
+        if (tvLigada) return;
 
         if (!popUpAtivado && popUpObject != null)
         {
-            // Modificar o diálogo antes do pop-up baseado na verificação da chave
-            int chaveIndex = EncontrarItemNoInventario(chaveArmarinho);
-            
+            // Modificar o diálogo antes do pop-up baseado na verificação da pilha
+            int pilhaIndex = EncontrarItemNoInventario(pilha);
+
             popUpObject.TriggerPopUp("Usar a pilha?",
-            () => TentarAbrirArmarinho(),
+            () => TentarLigarTv(),
             () => Debug.Log("O jogador recusou abrir o armarinho.")
         );
-            
+
         }
     }
-    public void TentarAbrirArmarinho()
+    public void TentarLigarTv()
     {
-        int chaveIndex = EncontrarItemNoInventario(chaveArmarinho);
+        int pilhaIndex = EncontrarItemNoInventario(pilha);
 
-        if (chaveIndex != -1)
+        if (pilhaIndex != -1)
         {
-            playerInventory.RemoveItem(chaveIndex, 5); // Remove uma unidade da chave
-            playerInventory.AddItem(recompensa, quantidadeRecompensa);
-            armarinhoAberto = true;
+            playerInventory.RemoveItem(pilhaIndex, 1); // Remove uma unidade da pilha
+            tvLigada = true;
             dialogueData = interacaoPosRecompensa;
             GameEvents.Instance.StartDialogue(posRecompensa);
             //GameEvents.Instance.OnFinishDialogue += DialogueEnded;
@@ -68,8 +65,8 @@ public class Armarinho : NPCs
         return -1; // Retorna -1 caso o item não seja encontrado
     }
 
-    private void ClosePopUp() 
+    private void ClosePopUp()
     {
-        Debug.Log("Sem chave");
+        Debug.Log("Sem pilha");
     }
 }
